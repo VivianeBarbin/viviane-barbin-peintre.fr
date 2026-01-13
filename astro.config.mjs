5757716f16f67aa1bde80a5f0df1f99209b0ea09
bap <baptiste.chenin.dev@gmail.com>
2026-01-13 11:10:13 +0100

    initial commit: - $ pnpm create astro@latest, - $ pnpm create sanity@latest, - $ pnpm dlx astro add @sanity/astro @astrojs/react, - create env.d.ts file - update astro.config.mjs - update ts.config.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+        projectId: 'x31r8s87',
+        dataset: 'local_dev',
+        useCdn: false,
+        studioBasePath: '/admin',
+        apiVersion: "2026-01-13"
+        
+      }), react()]
+});
